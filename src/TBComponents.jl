590c9a74bd8c4fc466cedd2139393d5e3347d4bf
Graham Stark <graham.stark@virtual-worlds.biz>
2018-10-11 23:04:49 +0100

    Start of reweighting module

--- src/TBComponents.jl
+++ src/TBComponents.jl
@@ -1,9 +1,11 @@
+"
 
+"
 module TBComponents
 
 export makegini, makepoverty, makeinequality, binify
 export DEFAULT_ATKINSON_ES,DEFAULT_ENTROPIES, DEFAULT_FGT_ALPHAS
 
 include("poverty_inequality.jl")
-
+include("reweighter.jl")
 end # module
