b0e82c3633552e06baed428f71026bb8f5347a4d
Graham Stark <graham.stark@virtual-worlds.biz>
2018-10-11 23:08:50 +0100

    reweighter

--- src/TBComponents.jl
+++ src/TBComponents.jl
@@ -5,7 +5,9 @@ module TBComponents
 
 export makegini, makepoverty, makeinequality, binify
 export DEFAULT_ATKINSON_ES,DEFAULT_ENTROPIES, DEFAULT_FGT_ALPHAS
+export DistanceFunctionType
 
 include("poverty_inequality.jl")
 include("reweighter.jl")
+
 end # module
--- src/reweighter.jl
+++ src/reweighter.jl
@@ -1,3 +1,4 @@
 "
 
 "
+@enum DistanceFunctionType chi_square d_and_s_type_a d_and_s_type_b constrained_chi_square d_and_s_constrained
