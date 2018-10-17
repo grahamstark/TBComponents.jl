a3b13448b5878f259a80ec78046b07903bf803ca
Graham Stark <graham.stark@virtual-worlds.biz>
2018-10-17 23:18:20 +0100

    added bc stuff to exports

--- src/TBComponents.jl
+++ src/TBComponents.jl
@@ -17,9 +17,12 @@ export calc_indirect, IndirResult
 export calctaxdue, TaxResult
 
 # budget constraint stuff
+export makebc,BCSettings,DEFAULT_SETTINGS
+export Point2DG,Point2D,BudgetConstraint
 
 include("poverty_inequality.jl")
 include("reweighter.jl")
 include("taxcalcs.jl")
+include( "piecewise_linear_generator.jl")
 
 end # module
