40db41714c811b97f50f8c027d9867eb7c2a0125
Graham Stark <graham.stark@virtual-worlds.biz>
2018-10-17 13:06:09 +0100

    Testing modified copying

--- test/minitb.jl
+++ test/minitb.jl
@@ -1,9 +1,28 @@
 
 @enum NetType  NetIncome TotalTaxes BenefitsOnly
 
+# experiment with types
+const NullableFloat = Union{Missing,Float64}
+const NullableInt = Union{Missing,Integer}
+const NullableArray = Union{Missing,Array{Float64}}
+
 struct Person
         wage :: Float64
         age  :: Integer
+end
+
+const DEFAULT_PERSON = Person( 1_000.0, 40 )
+
+function modifiedcopy(
+   copyFrom :: Person;
+   wage :: NullableFloat = missing,
+   age  :: NullableInt = missing
+   ) :: Person
+
+   Person(
+      wage !== missing ? wage : copyFrom.wage,
+      age !== missing ? age : copyFrom.age
+   )
 end
 
 struct Parameters
@@ -31,32 +50,28 @@ struct Parameters
    end
 end
 
-# experiment with types
-const NullableFloat = Union{Missing,Float64}
-const NullableArray = Union{Missing,Array{Float64}}
-
 function modifiedcopy(
-   ;
-   copyFrom :: Parameters,
+   copyFrom :: Parameters;
    it_allow :: NullableFloat = missing,
-   it_rate  :: NullableArray = missing},
-   it_band  :: NullableArray = missing},
+   it_rate  :: NullableArray = missing,
+   it_band  :: NullableArray = missing,
 
    benefit1 :: NullableFloat = missing,
    benefit2 :: NullableFloat = missing,
    ben2_l_limit :: NullableFloat = missing,
    ben2_u_limit :: NullableFloat = missing
    ) :: Parameters
-begin
+
+   x = it_allow !== missing ? it_allow : copyFrom.it_allow
    Parameters(
-      it_allow = it_allow !== missing ? it_allow: copyFrom.it_allow,
+      it_allow = it_allow !== missing ? it_allow : copyFrom.it_allow,
       it_rate  = it_rate  !== missing ? it_rate : copyFrom.it_rate,
       it_band  = it_band  !== missing ? it_band : copyFrom.it_band,
 
-      benefit1 = benefit1 !== missing ? benefit1 : copyFrom.benefit1,missing,
-      benefit2 = benefit2 !== missing ? benefit2 : copyFrom.benefit2,missing,
-      ben2_l_limit = ben2_l_limit !== missing ? ben2_l_limit : copyFrom.ben2_l_limit,missing,
-      ben2_u_limit = ben2_u_limit !== missing ? ben2_u_limit : copyFrom.ben2_u_limit,missing
+      benefit1 = benefit1 !== missing ? benefit1 : copyFrom.benefit1,
+      benefit2 = benefit2 !== missing ? benefit2 : copyFrom.benefit2,
+      ben2_l_limit = ben2_l_limit !== missing ? ben2_l_limit : copyFrom.ben2_l_limit,
+      ben2_u_limit = ben2_u_limit !== missing ? ben2_u_limit : copyFrom.ben2_u_limit
    )
 end
 
