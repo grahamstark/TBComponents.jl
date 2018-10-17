dc6704cf2b9a2377420dd46ac7f39ee792b250bb
Graham Stark <graham.stark@virtual-worlds.biz>
2018-10-17 18:25:16 +0100

    Basic tests of indir stuff

--- src/taxcalcs.jl
+++ src/taxcalcs.jl
@@ -9,32 +9,40 @@ struct TaxResult{T<:Real}
    end_band :: Integer
 end
 
+struct IndirResult{T<:Real}
+   factor_cost :: T
+   vat         :: T
+   addvalorem  :: T
+   specific    :: T
+   total       :: T
+end
+
 function uprate!(
-   x         <: Real,
-   uprate_by <: Real,
-   next      <: Real = 0.0 )
+   x         :: Real,
+   uprate_by :: Real,
+   next      :: Real = 0.0 )
    # TODO
 end
 
 function uprate!(
-   bands : RateBands,
-   uprate_by <: Real,
-   next      <: Real = 0.0 )
+   bands     :: RateBands,
+   uprate_by :: Real,
+   next      :: Real = 0.0 )
    # TODO
 end
 
 function stepped_tax_calculation(
-   taxable <: Real,
-   rates   <: RateBands,
-   bands   <: RateBands ) :: Tax_Result
+   taxable :: Real,
+   rates   :: RateBands,
+   bands   :: RateBands ) :: Tax_Result
       # TODO
 
 end
 
 function calc_tax_due(
-      taxable <: Real,
-      rates   <: RateBands,
-      bands   <: RateBands ) :: Tax_Result
+      taxable :: Real,
+      rates   :: RateBands,
+      bands   :: RateBands ) :: Tax_Result
       due = 0.0
       mr  = 0.0
       remaining = taxable
@@ -53,72 +61,66 @@ function calc_tax_due(
       Tax_Result( due, i )
 end
 
-struct IndirResult{T<:Real}
-   vat         :: T
-   addvalorem  :: T
-   specific    :: T
-   total       :: T
-end
-
 #
 # Factor cost (tax exclusive cost) given VAT, per unit tax and ad valorem tax).
 #
-# All inputs assumed to be vectors/1 dimension arrays of the same length
 # @param selling_price per unit in some currency
 # @param advalorem - tax as % of final selling price as 0.2 for 20%, for instance
 # @param vat tax as % of input costs (including advalorem and specific duties) 0.175 for 17.5% e.g.
 # @param
 #
-
-function factor_cost(
-   selling_price <: Real,
-   vat           <: Real,
-   advalorem     <: Real,
-   specific      <: Real ) <: Real
+function calc_factor_cost(
+   ;
+   selling_price :: Real,
+   vat           :: Real,
+   advalorem     :: Real,
+   specific      :: Real ) :: Real
 
    p = selling_price
    v = vat
    a = advalorem
    s = specific
-   ( p * ( 1.0 - a - ( v * a ))/( 1.0 + v ) ) - s;
+   return ( p * ( 1.0 - a - ( v * a ))/( 1.0 + v ) ) - s;
 end
 
 function calc_indir_components_per_unit(
    ;
-   selling_price <: Real,
-   vat           <: Real,
-   advalorem     <: Real,
-   specific      <: Real ) :: IndirResult
+   selling_price :: Real,
+   vat           :: Real,
+   advalorem     :: Real,
+   specific      :: Real ) :: IndirResult
    factor_cost = calc_factor_cost(
       selling_price = selling_price,
-      vat         = vat,
-      advalorem   = advalorem,
-      specific    = specific );
+      vat           = vat,
+      advalorem     = advalorem,
+      specific      = specific );
 
-   add = advalorem*selling_price
-   vat_due = vat * factor_cost
-   spec = specific
-   total = vat_due * add * spec
-   IndirResult( vat_due, add, specific, total )
+   add     = advalorem * selling_price
+   vat_due = vat * (factor_cost+add+specific)
+   total   = vat_due + add + specific
+   IndirResult( factor_cost, vat_due, add, specific, total )
 end
 
 function calc_indirect(
    ;
-   expenditure   <: Real,
-   selling_price <: Real,
-   vat           <: Real,
-   advalorem     <: Real,
-   specific      <: Real ) :: IndirResult
+   expenditure   :: Real,
+   selling_price :: Real,
+   vat           :: Real,
+   advalorem     :: Real,
+   specific      :: Real ) :: IndirResult
    per_unit = calc_indir_components_per_unit(
       selling_price = selling_price,
       vat           = vat,
       advalorem     = advalorem,
       specific      = specific )
-      q = expenditure / selling_price
-      IndirResult(
-         q*per_unit.vat,
-         q*per_unit.add,
-         q*per_unit.spec,
-         q*(per_unit.vat+per_unit.spec+per_unit.add)
-      )
+   q = expenditure / selling_price
+   println( "q=$q")
+   println( "per_unit = $per_unit")
+   IndirResult(
+      q*per_unit.factor_cost,
+      q*per_unit.vat,
+      q*per_unit.addvalorem,
+      q*per_unit.specific,
+      q*(per_unit.vat + per_unit.specific + per_unit.addvalorem)
+   )
 end
