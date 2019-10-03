46553cc31bc9b04760e44597dc1067b7ef8b31d5
Graham Stark <graham.stark@virtual-worlds.biz>
2019-10-03 10:31:27 +0100

    xx

--- src/poverty_inequality.jl
+++ src/poverty_inequality.jl
@@ -349,7 +349,7 @@ into numbins groups in a form suitable for
 e.g. a Gini curve.
 * col1 is cumulative population,
 *    2 cumulative income/whatever,
-*    3 threshold income level. 
+*    3 threshold income level.
 """
 function binify(
     rawdata   :: Array{<:Real, 2 },
